830d9f896677adb5a8a215c27abbd2efa21f9e5d
yasith <wimukthiyasith@gmail.com>
2023-02-09 20:47:43 +0530

    change login api call

--- src/components/register/SignUp.js
+++ src/components/register/SignUp.js
@@ -67,6 +67,8 @@ function SignUp(props) {
         "Content-Type": "application/json",
       },
     })
+        .then(response => response.data)
+        .catch(error => console.log(error));
   };
 
   const createCometChatAccount = async ({ id, fullname, avatar }) => {
