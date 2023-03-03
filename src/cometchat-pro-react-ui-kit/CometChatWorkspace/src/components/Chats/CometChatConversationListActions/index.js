e746f650829963d4260fad2309a0e971d364f5c4
yasith <wimukthiyasith@gmail.com>
2023-03-03 22:27:29 +0530

    change chat controls

--- src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/Chats/CometChatConversationListActions/index.js
+++ src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/Chats/CometChatConversationListActions/index.js
@@ -14,60 +14,60 @@ import loadingIcon from "./resources/progress.svg";
 import deleteIcon from "./resources/delete.svg";
 
 class CometChatConversationListActions extends React.PureComponent {
-	static contextType = CometChatContext;
+    static contextType = CometChatContext;
 
-	constructor(props) {
-		super(props);
-		this._isMounted = false;
-		this.state = {
-			deleteInProgress: false,
-		};
-	}
+    constructor(props) {
+        super(props);
+        this._isMounted = false;
+        this.state = {
+            deleteInProgress: false,
+        };
+    }
 
-	componentDidMount() {
-		this._isMounted = true;
-	}
+    componentDidMount() {
+        this._isMounted = true;
+    }
 
-	componentWillUnmount() {
-		this._isMounted = false;
-	}
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
 
-	toggleTooltip = (event, flag) => {
-		const elem = event.target;
+    toggleTooltip = (event, flag) => {
+        const elem = event.target;
 
-		if (flag) {
-			elem.setAttribute("title", elem.dataset.title);
-		} else {
-			elem.removeAttribute("title");
-		}
-	};
+        if (flag) {
+            elem.setAttribute("title", elem.dataset.title);
+        } else {
+            elem.removeAttribute("title");
+        }
+    };
 
-	deleteConversation = event => {
+    deleteConversation = event => {
 
-		this.props.actionGenerated(enums.ACTIONS["DELETE_CONVERSATION"], this.props.conversation);
-		event.stopPropagation();
-	};
+        this.props.actionGenerated(enums.ACTIONS["DELETE_CONVERSATION"], this.props.conversation);
+        event.stopPropagation();
+    };
 
-	render() {
-		const deleteConversation = (
-			<li>
-				<button
-				type="button"
-				css={groupButtonStyle(this.state.deleteInProgress, loadingIcon, deleteIcon)}
-				className="group__button button__delete"
-				data-title={Translator.translate("DELETE", this.context.language)}
-				onMouseEnter={event => this.toggleTooltip(event, true)}
-				onMouseLeave={event => this.toggleTooltip(event, false)}
-				onClick={this.deleteConversation} />
-			</li>
-		);
+    render() {
+        const deleteConversation = (
+            <li>
+                <button
+                    type="button"
+                    css={groupButtonStyle(this.state.deleteInProgress, loadingIcon, deleteIcon)}
+                    className="group__button button__delete"
+                    data-title={Translator.translate("DELETE", this.context.language)}
+                    onMouseEnter={event => this.toggleTooltip(event, true)}
+                    onMouseLeave={event => this.toggleTooltip(event, false)}
+                    onClick={this.deleteConversation}/>
+            </li>
+        );
 
-		return (
-			<ul css={conversationActionStyle(this.context)} className="list__item__actions">
-				{deleteConversation}
-			</ul>
-		);
-	}
+        return (
+            <ul css={conversationActionStyle(this.context)} className="list__item__actions">
+                {deleteConversation}
+            </ul>
+        );
+    }
 }
 
 export {CometChatConversationListActions};
