18db51c4a287b0761e05f779060d4909892eeec8
yasith <wimukthiyasith@gmail.com>
2023-03-13 22:24:31 +0530

    change call controls

--- src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/Groups/CometChatGroupList/style.js
+++ src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/Groups/CometChatGroupList/style.js
@@ -1,5 +1,5 @@
 export const groupWrapperStyle = (props, theme) => {
-    
+
     const borderStyle = (props._parent === "") ? {
         border: `1px solid ${theme.borderColor.primary}`
     } : {};
@@ -31,32 +31,32 @@ export const groupWrapperStyle = (props, theme) => {
 
 export const groupHeaderStyle = theme => {
 
-	return {
-		padding: "16px",
-		position: "relative",
-		display: "flex",
-		justifyContent: "space-between",
-		alignItems: "center",
-		borderBottom: `1px solid ${theme.borderColor.primary}`,
-		height: "70px",
-	};
+    return {
+        padding: "16px",
+        position: "relative",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottom: `1px solid ${theme.borderColor.primary}`,
+        height: "70px",
+    };
 };
 
 export const groupHeaderCloseStyle = (img, theme) => {
 
-	const mq = [...theme.breakPoints];
+    const mq = [...theme.breakPoints];
 
-	return {
-		cursor: "pointer",
-		display: "none",
-		mask: `url(${img}) left center no-repeat`,
-		backgroundColor: `${theme.primaryColor}`,
-		height: "24px",
-		width: "33%",
-		[`@media ${mq[0]}`]: {
-			display: "block!important",
-		},
-	};
+    return {
+        cursor: "pointer",
+        display: "none",
+        mask: `url(${img}) left center no-repeat`,
+        backgroundColor: `${theme.primaryColor}`,
+        height: "24px",
+        width: "33%",
+        [`@media ${mq[0]}`]: {
+            display: "block!important",
+        },
+    };
 };
 
 export const groupHeaderTitleStyle = (props) => {
@@ -82,26 +82,26 @@ export const groupHeaderTitleStyle = (props) => {
 
 export const groupAddStyle = (img, theme) => {
 
-	return {
-		height: "24px",
-		cursor: "pointer",
-		i: {
-			display: "inline-block",
-			width: "24px",
-			height: "24px",
-			mask: `url(${img}) center center no-repeat`,
-			backgroundColor: `${theme.primaryColor}`,
-		},
-	};
+    return {
+        height: "24px",
+        cursor: "pointer",
+        i: {
+            display: "inline-block",
+            width: "24px",
+            height: "24px",
+            mask: `url(${img}) center center no-repeat`,
+            backgroundColor: `${theme.primaryColor}`,
+        },
+    };
 };
 
 export const groupSearchStyle = () => {
-    
+
     return {
         margin: "16px",
         position: "relative",
         borderRadius: "8px",
-		boxShadow: "rgba(20, 20, 20, 0.04) 0 0 0 1px inset",
+        boxShadow: "rgba(20, 20, 20, 0.04) 0 0 0 1px inset",
         backgroundColor: "rgba(20, 20, 20, 0.04)",
         height: "35px",
         display: "flex",
@@ -112,14 +112,14 @@ export const groupSearchStyle = () => {
 
 export const groupSearchButtonStyle = (img, context) => {
 
-	return {
-		width: "30px",
-		height: "100%",
-		padding: "8px 0 8px 8px",
-		cursor: "default",
-		mask: `url(${img}) 10px center no-repeat`,
-		backgroundColor: `${context.theme.secondaryTextColor}!important`,
-	};
+    return {
+        width: "30px",
+        height: "100%",
+        padding: "8px 0 8px 8px",
+        cursor: "default",
+        mask: `url(${img}) 10px center no-repeat`,
+        backgroundColor: `${context.theme.secondaryTextColor}!important`,
+    };
 };
 
 export const groupSearchInputStyle = () => {
@@ -136,7 +136,7 @@ export const groupSearchInputStyle = () => {
 }
 
 export const groupMsgStyle = () => {
-    
+
     return {
         overflow: "hidden",
         width: "100%",
@@ -150,16 +150,16 @@ export const groupMsgStyle = () => {
 
 export const groupMsgTxtStyle = theme => {
 
-	return {
-		margin: "0",
-		minHeight: "36px",
-		color: `${theme.color.secondary}`,
-		fontSize: "20px!important",
-		fontWeight: "600",
-		lineHeight: "30px",
-		wordWrap: "break-word",
-		padding: "0 16px",
-	};
+    return {
+        margin: "0",
+        minHeight: "36px",
+        color: `${theme.color.secondary}`,
+        fontSize: "20px!important",
+        fontWeight: "600",
+        lineHeight: "30px",
+        wordWrap: "break-word",
+        padding: "0 16px",
+    };
 };
 
 export const groupListStyle = () => {
